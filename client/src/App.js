601c37754f561e77a2937a5e50d952456c6a1902
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-07-25 17:43:47 -0400

    modified front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import styled, {ThemeProvider} from 'styled-components';
-import ParticleComponent from './components/subComponents/Particles';
-
-const DarkTheme = {
-
-  body:"#000000",
-  text:"#FCF6F4",
-  fontFamily:"'Source Sans Pro', sans-serif",
-  textRgba : "252, 246, 244",
-  bodyRgba:"0,0,0",
-}
-
-const Box = styled.div`
-background-color: ${props => props.theme.body};
-width: 100vw;
-height:100vh;
-position: relative;
-overflow: hidden;
-`
+import '../src/assets/css/style.css'
 
 function App() {
   return (
-    <ThemeProvider theme={DarkTheme}>
-    <Box>
-      <ParticleComponent theme='dark' />
-      <div className="App">
-        <header className="App-header">
+      <div>
+        <header>
           <p>
             OG-SNKRS
           </p>
           <p>Can you kick it?</p>
         </header>
       </div>
-    </Box>
-    </ThemeProvider>
   );
 }
 
