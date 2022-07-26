96948755e6d37ed01bdf72f8c005d690784f34e5
Thuy Nguyen <ttruong06@gmail.com>
2022-07-26 11:42:24 -0400

    working on client navbar routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,69 +2,53 @@ import React from "react";
 import logo from "../src/assets/images/nike_sneakers-brandlogo.net.png";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
 
-import Nav from "./components/Nav";
-import SearchShoes from './components/pages/SearchShoes';
-import SavedShoes from './components/pages/SavedShoes';
+import NavApp from "./components/Nav";
+import SignupForm from "./components/Signup";
+import LoginForm from "./components/Login";
+// import Shop from "./components/Shop";
+import Home from "./components/pages/Home";
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
-
-    <div>
-       <Nav />
-        
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>Can you kick it?</p>
-            </header>
-          </div>
-    </div>
-
-    
-    // <ApolloProvider>
-    //   <Router>
-    //     <>
-    //       <Nav />
-    //       <div className="App">
-    //         <header className="App-header">
-    //           <img src={logo} className="App-logo" alt="logo" />
-    //           <p>Can you kick it?</p>
-    //         </header>
-    //       </div>
-    //       <Switch>
-    //       <Route exact path='/' component={SearchShoes} />
-    //       <Route exact path='/saved' component={SavedShoes} />
-    //       <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-    //       </Switch>
-    //     </>
-    //   </Router>
-    // </ApolloProvider>
-    
-       
-        
+    <Router>
+      <NavApp />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Can you kick it?</p>
+        </header>
+      </div>
+      {/* <div className="container"> */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/shop" element={<Shop />} /> */}
+          <Route path="/signupform" component={SignupForm} />
+          <Route path="/loginform" component={LoginForm} />
+        </Switch>
+      {/* </div> */}
+    </Router>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,43 +1,68 @@
-import React, {useState, useEffect } from "react";
-import './Nav.css'
+import React from "react";
+import "./Nav.css";
+import { Link } from "react-router-dom";
+
+function NavApp() {
+
+  return (
+    <ul className="nav-list">
+      <li className="nav-items"><Link to="/">Home</Link></li>
+      <li className="nav-items"><Link to="/shop">Shop</Link></li>
+      <li className="nav-items"><Link to="/signupform">Sign Up</Link></li>
+      <li className="nav-items"><Link to="/loginform">Log In</Link></li>
+    </ul>
+  );
 
 
-function Nav() {
-  const [toggleMenu, setToggleMenu] = useState(false)
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  // const [toggleMenu, setToggleMenu] = useState(false)
+  // const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
-  const toggleNav = () => {
-    setToggleMenu(!toggleMenu)
-  }
+  // const toggleNav = () => {
+  //   setToggleMenu(!toggleMenu)
+  // }
 
-  useEffect(() => {
-    const changeWidth = () => {
-      setScreenWidth(window.innerWidth);
-    }
+  // useEffect(() => {
+  //   const changeWidth = () => {
+  //     setScreenWidth(window.innerWidth);
+  //   }
 
-    window.addEventListener('resize', changeWidth)
+  //   window.addEventListener('resize', changeWidth)
 
-    return () => {
-        window.removeEventListener('resize', changeWidth)
-    }
+  //   return () => {
+  //       window.removeEventListener('resize', changeWidth)
+  //   }
 
-  }, [])
+  // }, [])
 
-return (
-    <nav>
-    {(toggleMenu || screenWidth > 500) && (
-        <ul className="nav-list">
-            <a href="/" ><li className="nav-items">Home</li></a>
-            <li className="nav-items">Shop</li>
-            <a href="/signup" ><li className="nav-items">Sign Up</li></a>
-            <li className="nav-items">Log In</li>
-        </ul>
-        )}
-        <button onClick={toggleNav}className="btn">Button</button>
-    </nav> 
+    // <nav>
+    //   <ul className="nav-list">
+    //     <CustomLink to="/">
+    //       <li className="nav-items">Home</li>
+    //     </CustomLink>
+    //     <CustomLink to="/shop">
+    //       <li className="nav-items">Shop</li>
+    //     </CustomLink>
+    //     <CustomLink className="nav-items" to="/signup">
+    //       Sign Up</CustomLink>
+    //     <CustomLink to="/login">
+    //       <li className="nav-items">Login</li>
+    //     </CustomLink>
+    //   </ul>
+    // </nav>
 
-);
 
+    // <nav>
+    // {(toggleMenu || screenWidth > 500) && (
+    //     <ul className="nav-list">
+    //         <a href="/" ><li className="nav-items">Home</li></a>
+    //         <li className="nav-items">Shop</li>
+    //         <a href="/signup" ><li className="nav-items">Sign Up</li></a>
+    //         <li className="nav-items">Log In</li>
+    //     </ul>
+    //     )}
+    //     <button onClick={toggleNav}className="btn">Button</button>
+    // </nav>
+  
 }
 
-export default Nav;
+export default NavApp;
