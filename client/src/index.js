2758a16d587c026ab054bb1d54c8548f397e21bd
Thuy Nguyen <ttruong06@gmail.com>
2022-07-26 19:25:27 -0400

    modified links

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'; 
 
 const rootElement = document.getElementById("root");
+
+const client = new ApolloClient({
+  uri: 'link: authLink.concat(httpLink)',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   rootElement
 );
